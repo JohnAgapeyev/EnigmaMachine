e80beedd930fd34d67c4e198839b318a5ddf103b
JohnAgapeyev <jagapeyev@gmail.com>
2015-05-24 21:24:01 -0700

    Added more elements to the panel

--- src/machine/enigma/Enigma.java
+++ src/machine/enigma/Enigma.java
@@ -1,24 +1,20 @@
 package machine.enigma;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Enigma {
 
     private static Random rand = new Random();
     private static Rotor[] rotors = new Rotor[5];
     private static int rotorLength = rotors.length;
-    private static final Scanner scan = new Scanner(System.in);
 
     public Enigma() {
         for (int i = 0; i < rotorLength; i++) {
             rotors[i] = new Rotor();
         }
-        System.out.println("Please enter your message: ");
-        System.out.println(encode(scan.nextLine()));
     }
 
-    private static String encode(String message) {
+    public String encode(String message) {
         char[] messageLetters = message.toLowerCase().toCharArray();
         char[] rotorKey = rotors[rand.nextInt(rotorLength)].generate()
                 .toCharArray();
--- src/machine/enigma/EnigmaPanel.java
+++ src/machine/enigma/EnigmaPanel.java
@@ -1,10 +1,13 @@
 package machine.enigma;
 
 import javax.swing.JPanel;
-import java.awt.Graphics;
-import java.awt.Color;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class EnigmaPanel extends JPanel {
 
@@ -12,19 +15,46 @@ public class EnigmaPanel extends JPanel {
      * Serial ID for the Panel.
      */
     private static final long serialVersionUID = 7298201505806512569L;
-    
+
+    private JButton rotorChange;
     private JLabel outputMessage;
+    private JLabel rotor1;
+    private JLabel rotor2;
+    private JLabel rotor3;
     private JTextField message;
+    private Enigma machine;
 
     public EnigmaPanel() {
+        machine = new Enigma();
         outputMessage = new JLabel("yep");
+        rotor1 = new JLabel("0");
+        rotor2 = new JLabel("0");
+        rotor3 = new JLabel("0");
         message = new JTextField("", 5);
+        rotorChange = new JButton("Switch Rotors");
+        message.addActionListener(new EnigmaActionListener());
+        rotorChange.addActionListener(new EnigmaActionListener());
         add(outputMessage);
         add(message);
+        add(rotorChange);
+        add(rotor1);
+        add(rotor2);
+        add(rotor3);
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+    }
+
+    private class EnigmaActionListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (e.getSource() == message) {
+                outputMessage.setText(machine.encode(message.getText()));
+            } else if (e.getSource() == rotorChange) {
+                System.out.println("");
+            }
+        }
     }
 }
